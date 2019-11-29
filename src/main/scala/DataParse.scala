60eda0429dd2656df6e5809df93449574b7e3485
leejr <leejr@rams.colostate.edu>
2019-11-29 16:28:42 -0700

    Setting up the input data.

--- src/main/scala/DataParse.scala
+++ src/main/scala/DataParse.scala
@@ -8,10 +8,51 @@ object DataParse {
 //    val conf = new SparkConf().setAppName("censusAnalysis").setMaster("yarn")
     val sc = new SparkContext(conf)
 
+    //Getting all of the incident data that is needed for analysis
+    val incidentFile1 = sc.textFile("data/incidentData1")
+    val incidentFile2 = sc.textFile("data/incidentData2")
+
+    val incidentData1 = incidentFile1.map(value => {
+      val result = value.substring(1,value.length-1)
+      val data = result.split(",")
+
+      //zip code -- time, incident category, police district
+      (data(0), data(3)+","+data(15)+","+data(21))
+    })
+    val incidentData2 = incidentFile2.map(value => {
+      val result = value.substring(1,value.length-1)
+      val data = result.split(",")
+
+      //zip code -- time, incident category, police district
+      (data(0), data(3)+","+data(15)+","+data(21))
+    })
+
+    //Getting all of the census data that is needed for analysis
+    val censusFile1 = sc.textFile("data/censusData1")
+    val censusFile2 = sc.textFile("data/censusData2")
+
+    val censusData1 = censusFile1.map(value => {
+      val result = value.substring(8,value.length-2)
+      val data = result.split(",")
+
+      //zip code -- Total Population, aggregate income
+      (value.substring(1,6), data(0)+","+data(1237)+","+data(1504))
+    })
+
+    val censusData2 = censusFile2.map(value => {
+      val result = value.substring(8,value.length-2)
+      val data = result.split(",")
+
+      //zip code -- Total Population, aggregate income
+      (value.substring(1,6), data(0)+","+data(1237)+","+data(1504))
+    })
+
+
+
+
 
     val dataFile1 = sc.textFile("temp.txt")
-//    val dataFile2 = sc.textFile(args(2))
-
+    //    val dataFile2 = sc.textFile(args(2))
     dataFile1.foreach(value => println(value));
 
     var organizedData1 = dataFile1.map(value => (value.substring(1,6), value.substring(8, value.length-2)))
@@ -19,5 +60,7 @@ object DataParse {
     organizedData1.foreach(value => println(value))
 
     organizedData1 = organizedData1.map(value => (value._1, value._2))
+
+
   }
 }
