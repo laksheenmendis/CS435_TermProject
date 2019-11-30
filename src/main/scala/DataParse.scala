8b1add3bbb20cd1ac6d321dce872a448a9e66182
leejr <leejr@rams.colostate.edu>
2019-11-30 14:31:39 -0700

    Added a few comments.
    Added a draft of the final dataset.

--- src/main/scala/DataParse.scala
+++ src/main/scala/DataParse.scala
@@ -4,15 +4,15 @@ import org.apache.spark._
 object DataParse {
 
   def main(args: Array[String]) : Unit = {
-    val conf = new SparkConf().setAppName("censusAnalysis").setMaster("local")
-//    val conf = new SparkConf().setAppName("censusAnalysis").setMaster("yarn")
+//    val conf = new SparkConf().setAppName("censusAnalysis").setMaster("local")
+    val conf = new SparkConf().setAppName("censusAnalysis").setMaster("yarn")
     val sc = new SparkContext(conf)
 
     //Getting all of the incident data that is needed for analysis
-    val incidentFile1 = sc.textFile("data/testingData/incidents1")
-    //val incidentFile1 = sc.textFile(args(0))
-    val incidentFile2 = sc.textFile("data/testingData/incidents2.txt")
-    //val incidentFile2 = sc.textFile(args(1))
+//    val incidentFile1 = sc.textFile("data/testingData/incidents1")
+    val incidentFile1 = sc.textFile(args(0))
+//    val incidentFile2 = sc.textFile("data/testingData/incidents2.txt")
+    val incidentFile2 = sc.textFile(args(1))
 
     val incidentData1 = incidentFile1.map(value => {
       val result = value.substring(1,value.length-1)
@@ -29,13 +29,14 @@ object DataParse {
       (data(0), data(3)+";"+data(15)+";"+data(21))
     })
 
+    //merging the incident data
     val incidents = incidentData1.union(incidentData2)
 
     //Getting all of the census data that is needed for analysis
-    val censusFile1 = sc.textFile("data/censusData1")
-    //val censusFile1 = sc.textFile(args(2))
-    val censusFile2 = sc.textFile("data/censusData2")
-    //val censusFile2 = sc.textFile(args(3))
+//    val censusFile1 = sc.textFile("data/censusData1")
+    val censusFile1 = sc.textFile(args(2))
+//    val censusFile2 = sc.textFile("data/censusData2")
+    val censusFile2 = sc.textFile(args(3))
 
     val censusData1 = censusFile1.map(value => {
       val result = value.substring(8,value.length-2)
@@ -53,11 +54,13 @@ object DataParse {
       (value.substring(1,6), data(0)+";"+data(1237)+";"+data(1504))
     })
 
+    //Remove duplicate values
     val census = censusData1.union(censusData2).reduceByKey((value1, value2) => value1)
 
-
+    //Join the census and the incidents, key is zip code
     val joinedData = census.join(incidents)
 
+    //Doing some intermediate processing. Making the data easier to parse
     val intermediate = joinedData.reduceByKey((value1, value2) => {
       val censusPortion = value1._1
 
@@ -66,6 +69,7 @@ object DataParse {
       (censusPortion,result)
     })
 
+    //Getting the final data: zip code key, then census data, and overall incident report data
     val finalData = intermediate.map(value => {
       val censusPortion = "censusData: {" + value._2._1 + "}"
 
@@ -73,7 +77,7 @@ object DataParse {
       var num2 = 0
       var num3 = 0
       var num4 = 0
-
+      //Getting the number of incidents for each zip code, organized by 6 hour time slots
       val incidents = value._2._2.split("--")
       var index = 0
       while(index != incidents.length) {
@@ -107,7 +111,7 @@ object DataParse {
       (value._1, (censusPortion, result))
     })
 
-    finalData.saveAsTextFile("output")
-    //finalData.saveAsTextFile(args(4))
+//    finalData.saveAsTextFile("output")
+    finalData.saveAsTextFile(args(4))
   }
 }
